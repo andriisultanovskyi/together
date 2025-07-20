48072c0094fa2e30dd190b8c16aa62875c921df0
andriisultanovskyi <andrii.sultanovskyi@gmail.com>
2025-07-20 18:00:07 +0200

    adding test_click_pl_button, test_click_ru_button

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,26 +1,31 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException, NoSuchElementException, ElementClickInterceptedException
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 
 
 class BasePage:
     DEFAULT_TIMEOUT = 10
+
     def __init__(self, driver, timeout=10):
         self.driver = driver
         self.timeout = timeout
-
 
     def find_element(self, locator, timeout=DEFAULT_TIMEOUT):
         return WebDriverWait(self.driver, timeout).until(
             EC.presence_of_element_located(locator)
         )
 
-
     def wait_for_element_to_be_clickable(self, locator, timeout=DEFAULT_TIMEOUT):
         """Waits for the element to become clickable."""
         return WebDriverWait(self.driver, timeout).until(
             EC.element_to_be_clickable(locator)
+        )
+
+    def wait_for_element_visible(self, locator, timeout=DEFAULT_TIMEOUT):
+        """Waits for the element to be visible on the page."""
+        return WebDriverWait(self.driver, timeout).until(
+            EC.visibility_of_element_located(locator)
         )
 
     def wait_for_url_contains(self, text, timeout=DEFAULT_TIMEOUT):
@@ -36,10 +41,9 @@ class BasePage:
         return self.driver.current_url
 
     COOKIE_POPUP_CONTAINER = (
-    By.XPATH, "//div[@class='cky-consent-container' and @aria-label='Cenimy prywatność użytkowników']")
+        By.XPATH, "//div[@class='cky-consent-container' and @aria-label='Cenimy prywatność użytkowników']")
     COOKIE_ACCEPT_BUTTON = (
-    By.XPATH, ".//button[contains(@class, 'cky-btn-accept') and contains(text(), 'Akceptuj wszystko')]")
-
+        By.XPATH, ".//button[contains(@class, 'cky-btn-accept') and contains(text(), 'Akceptuj wszystko')]")
 
     def close_cookie_popup(self, timeout=None):
         if timeout is None:
